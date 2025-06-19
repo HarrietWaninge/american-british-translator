982b7b528a61bfffa82b2619d987a3d1726ea4bf
harriet.waninge <harriet.waninge@gmail.com>
2025-06-19 11:31:05 +0200

    edge case titles

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,7 @@ const britishOnly = require("./british-only.js");
 
 class Translator {
   constructor() {
+    this.locale = ["british-to-american", "american-to-british"];
     // make word/phases regex
     let americanRegexBits = Object.keys(americanOnly).concat(
       Object.keys(americanToBritishSpelling)
@@ -36,12 +37,18 @@ class Translator {
     );
 
     //make time rexex
-    this.britishTimeRegex = new RegExp("\\d?\\d\\.\\d{2}", "g");
-    this.americanTimeRegex = new RegExp("\\d?\\d:\\d{2}", "g");
+    this.britishTimeRegex = new RegExp(
+      "(?<hours>\\d?\\d)\\.(?<minutes>\\d{2})",
+      "g"
+    );
+    this.americanTimeRegex = new RegExp(
+      "(?<hours>\\d?\\d):(?<minutes>\\d{2})",
+      "g"
+    );
   }
 
-  translate(textString) {
-    console.log(this.findAllSuspects(textString));
+  translate(textString, locale) {
+    this.buildTranslationReturn(textString);
     return "We watched the soccer match for a while.";
   }
 
@@ -61,6 +68,46 @@ class Translator {
     return { american: americanBits, british: britishBits };
   }
 
+  getTranslations(textString, locale) {
+    let allSuspects = this.findAllSuspects(textString);
+    let translation;
+    if (locale == this.locale[1]) {
+      // american-to-british, find translations of american words
+      for (let i = 0; i < allSuspects.american.length; i++) {
+        //mijn probleem: de titles hoofdletters don't translate. stelletje fuckers. they did this on purpose. they knew this.
+        let lowerCaseAmerican = allSuspects.american[i].toLowerCase();
+        console.log("LC", lowerCaseAmerican);
+        if (lowerCaseAmerican.matchAll(this.americanTimeRegex)) {
+          //handle american time
+          translation = lowerCaseAmerican.replace(
+            this.americanTimeRegex,
+            "$<hours>.$<minutes>"
+          );
+        } else if (lowerCaseAmerican.match(this.americanTitlesRegex)) {
+          console.log("HOP!", lowerCaseAmerican);
+        } else {
+          translation =
+            americanOnly[lowerCaseAmerican] ||
+            americanToBritishSpelling[lowerCaseAmerican] ||
+            americanToBritishTitles[lowerCaseAmerican];
+        }
+
+        console.log(
+          "american:",
+          allSuspects.american[i],
+          "british:",
+          translation
+        );
+      }
+    }
+  }
+
+  buildTranslationReturn(textString) {
+    let translatedWords = this.getTranslations(
+      textString,
+      "american-to-british"
+    );
+  }
   escapeDots(stringsArray) {
     for (let i = 0; i < stringsArray.length; i++) {
       if (stringsArray[i].includes(".")) {
